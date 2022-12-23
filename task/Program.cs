9c80df14c25d61dcdecc8e990fc19397b89c648f
Boris Dovbysh <boris.dovbysh@gmail.com>
2022-12-23 17:48:30 +0300

    Add input in task

--- task/Program.cs
+++ task/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите количество строк в массиве: ");
+//int amountString = Convert.ToInt32(Console.ReadLine());
+string str = Console.ReadLine();
+if (int.TryParse(str, out int amountString))
+{
+
+}
+else
+{
+    Console.WriteLine("Введите целое число больше нуля");
+}
+if (amountString < 0)
+{
+    Console.WriteLine("Введите целое число больше нуля");
+}
+
+string [] inputArray = new string [amountString];
+for (int i = 0; i < amountString; i++)
+{
+    Console.Write($"Введите {i+1}-й элемент массива строк: ");
+    inputArray[i] = Console.ReadLine();
+}
+string arr = string.Join(", ", inputArray);
+Console.Write(arr);
